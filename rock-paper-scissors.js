9806d62dbbc4180f3830f3c5d4ad4abf1ad58491
bill <billmukenga01@gmail.com>
2023-03-07 15:46:02 +0300

    understanding of the problem

---
+++ rock-paper-scissors.js
@@ -0,0 +1,4 @@
+//randomly generate the computers choice out of a selection of rock, paper or scissors
+//ask the user for input
+//compare the two and see who wins the round
+//continue this process of playing the round and the best of 5 wins the game
