6bfedd0a09aeba330913eb824231fd75066a69f2
bill <billmukenga01@gmail.com>
2023-03-07 15:51:46 +0300

    wrote basic pseudocode

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,3 +2,18 @@
 //ask the user for input
 //compare the two and see who wins the round
 //continue this process of playing the round and the best of 5 wins the game
+
+
+//create a variable userWins
+//create a variable computerWins
+/*on of the two will increment if the user or the computer wins the round and the fisrt one to
+reach 3 wins the game
+*/
+
+// if userWins is equal to 3 exit game and console.log winner
+// else if computerWins is equal to 3 exit game and console.log winner
+//else continue playing
+
+//function to get the computer input randomly
+
+//function to get the users input using prompt 
