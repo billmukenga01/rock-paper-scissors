13d8072a3ef6d07eb6ab42143fae4f58894e3084
bill <billmukenga01@gmail.com>
2023-03-08 11:53:49 +0300

    completed conditional statement in oneRound func

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -38,27 +38,24 @@ function getComputerChoice(){
 
 function oneRound(playerSelection, computerSelection){
     if(playerSelection === 'rock' && computerSelection === 'rock'){
-        return "You Lose! Paper beats Rock";
+        return "Draw!";
     }else if(playerSelection === 'rock' && computerSelection === 'paper'){
         return "You Lose! Paper beats Rock";
     }else if(playerSelection === 'rock' && computerSelection === 'scissors'){
-        return "You Lose! Paper beats Rock";
+        return "You Win! Rock beats Scissors";
     }else if(playerSelection === 'paper' && computerSelection === 'rock'){
-        return "You Lose! Paper beats Rock";
+        return "You Win! Paper beats Rock";
     }else if(playerSelection === 'paper' && computerSelection === 'paper'){
-        return "You Lose! Paper beats Rock";
+        return "Draw!";
     }else if(playerSelection === 'paper' && computerSelection === 'scissors'){
-        return "You Lose! Paper beats Rock";
+        return "You Lose! Scissors beats Paper";
     }else if(playerSelection === 'scissors' && computerSelection === 'rock'){
-        return "You Lose! Paper beats Rock";
+        return "You Lose! Rock beats scissors";
     }else if(playerSelection === 'scissors' && computerSelection === 'paper'){
-        return "You Lose! Paper beats Rock";
+        return "You Win! Scissors beats Paper";
     }else{
-        return "You Lose! Paper beats Rock";
+        return "Draw!";
     }
-
-
-
 }
 
 oneRound(playerChoice, computerChoice);
